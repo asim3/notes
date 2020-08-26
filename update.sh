31ce01d7329931fed9020caac83e2fa390efdfec
asim3 <asimwebapps@gmail.com>
2020-08-26 22:30:24 +0300

    update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 function update_paths {
     for dir in $1/* ; do
         if [[ $dir != *"init.txt" ]]; then
-            echo "..link.. $dir";
+            echo "..link.. $(echo "$dir" | sed -e 's/.\///')";
             echo $(echo "$dir" | sed -e 's/.*\///' -e 's/\..*//' -e 's/_/ /');
         fi
 
@@ -13,6 +13,4 @@ function update_paths {
     done;
 }
 
-cd ./data
 update_paths "."
-cd ..
