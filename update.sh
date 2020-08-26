e0e1ec5f00a460fc6b414dfb3bcd1078e26ec6de
asim3 <asimwebapps@gmail.com>
2020-08-26 17:25:48 +0300

    update bash functions

--- update.sh
+++ update.sh
@@ -1,1 +1,18 @@
-tree -J --noreport --dirsfirst ./data > ./static/paths.json
+#!/bin/bash
+
+function update_paths {
+    for dir in $1/* ; do
+        if [[ $dir != *"init.txt" ]]; then
+            echo "..link.. $dir";
+            echo $(echo "$dir" | sed -e 's/.*\///' -e 's/\..*//' -e 's/_/ /');
+        fi
+
+        if [ -d $dir ]; then
+            update_paths $dir > $dir/init.txt
+        fi
+    done;
+}
+
+cd ./data
+update_paths "."
+cd ..
