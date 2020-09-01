341b6be67092d297a5e969007263400e89eba3b7
asim3 <asimwebapps@gmail.com>
2020-09-01 11:38:12 +0300

    update parse function .txt or .md or .html

--- static/js/main.js
+++ static/js/main.js
@@ -136,7 +136,7 @@ const fetch_path = function (path, back = false) {
             if (response.ok) {
                 response.text()
                     .then(function (text) {
-                        const body_text = path.slice(-3) === ".md" ? markdownit().render(text) : parse_tags(text)
+                        const body_text = path.slice(-3) === ".md" ? markdownit().render(text) : path.slice(-5) === ".html" ? text : parse_tags(text)
                         if (!back) {
                             const new_href = website_href + "?path=" + path
                             window.history.pushState(path, null, new_href)
