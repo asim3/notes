680cec015d2a40e6a71483ce904349c24c732816
asim3 <asimwebapps@gmail.com>
2020-10-31 17:10:08 +0300

    update push to prod

--- push_to_prod.sh
+++ push_to_prod.sh
@@ -1,9 +1,13 @@
 git switch production
 git pull origin master
+
+./update.sh
+
 git add .
 
-if git add .; then
-    echo aaa
-else
-    echo bbb
-fi;
+git config --global user.name "asim3"
+git config --global user.email "asim3"
+
+git commit -m "auto update init.html"
+
+git push origin production
