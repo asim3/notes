a9549bb94de0df5f23b91d04388ba2a7a68507ce
asim3 <asimwebapps@gmail.com>
2020-12-28 23:38:48 +0300

    update push to production...

--- push_to_prod.sh
+++ push_to_prod.sh
@@ -1,3 +1,6 @@
+git config --global user.name "asim3"
+git config --global user.email "asim3"
+
 git checkout -b production
 
 git merge master
@@ -5,9 +8,6 @@ git merge master
 source ./update.sh
 
 git add .
-
-git config --global user.name "asim3"
-git config --global user.email "asim3"
 
 git commit -m "auto update init.html"
 
